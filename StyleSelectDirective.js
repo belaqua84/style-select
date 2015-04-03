b5c8d70b6260b970dd584168cf8844a9665ebecb
Tracy Hamer <tracy.hamer@gmail.com>
2015-04-03 10:28:33 -0700

    adding readme

---
+++ StyleSelectDirective.js
@@ -0,0 +1,18 @@
+myApp.directive('styleSelect', [function(){
+		return {
+  			restrict: 'A',
+        scope: { 
+            items: '=', 
+            ngModel: '='
+        },
+        templateUrl: 'styleSelectDirective.html',
+        link: function(scope, element, attr){
+            scope.selectClass = attr.selectclass;
+            scope.whoAmI = attr.whoami;
+            scope.labelMe = (attr.whoami).charAt(0).toUpperCase() + (attr.whoami).slice(1); 
+            if(attr.labelme){
+                scope.labelMe = attr.labelme;  
+            }
+        }
+		}
+}]);
